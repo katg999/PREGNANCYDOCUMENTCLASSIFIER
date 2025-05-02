339f1ec0bc3ad0ca174647b836b84b6d17ab0b03
braingilbert <briangilbert@DESKTOP-BOD1Q8N>
2025-05-02 11:07:17 +0300

    UpdatedEndpointExposuresInTheSpaces_Services.pyFile

--- classifier_service.py
+++ classifier_service.py
@@ -2,7 +2,7 @@ import os
 import requests
 from tenacity import retry, stop_after_attempt, wait_exponential
 
-HF_API_URL = "https://h5juq0gnjnavay71.us-east-1.aws.endpoints.huggingface.cloud"
+HF_API_URL = os.getenv("MODEL_ENDPOINT")
 DOCUMENT_TYPES = [
     "ultrasound report",
     "blood test results",
